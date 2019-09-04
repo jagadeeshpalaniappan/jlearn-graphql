025d80926d1d9c97794d3fb1c71e673242525afe
Jagadeesh Palaniappan <jagadeeshthegeek@gmail.com>
2019-09-04 11:39:26 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const expressGraphQL = require("express-graphql");
 
-// const schema = require("./booksAndAuthorsSchema.js");
-const schema = require("./jlearn/2-advanced-query.js");
+// const schema = require("./all.js");
+// const schema = require("./jlearn/1-simple-query.js");
+// const schema = require("./jlearn/2-advanced-query.js");
+// const schema = require("./jlearn/3-advanced-query.js");
+const schema = require("./jlearn/4-simple-mutations.js");
 
 const expressGraphqlMiddleWare = expressGraphQL({
   schema: schema,
